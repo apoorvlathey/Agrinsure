0b2073562135183c07f1560e321606382eb74a33
CodinMaster <latheyapoorv@gmail.com>
2021-12-16 18:16:31 +0530

    update contracts

--- contracts/FarmInsure.sol
+++ contracts/FarmInsure.sol
@@ -1,20 +1,9 @@
-pragma solidity 0.4.24;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
 
-import "../node_modules/chainlink/contracts/ChainlinkClient.sol";
-import "./DateTime.sol";
-import "./strings.sol";
+import "./Oracle.sol";
 
-contract Agrinsure is ChainlinkClient, DateTime{
-    using strings for *;
-
-    uint256 private oraclePaymentAmount;
-    bytes32 private jobId;
-
-    bool public resultReceived;
-    //int256 public result;
-
-    uint private claimPolicyId;
-
+contract FarmInsure {
     //for BaseMin to BaseMax -> BasePayout% . for > Max -> MaxPayout%
     uint8 constant floodBaseMin = 10;
     uint8 constant floodBaseMax = 15;
@@ -40,7 +29,7 @@ contract Agrinsure is ChainlinkClient, DateTime{
 
     struct policy {
         uint policyId;
-        address user;
+        address payable user;
         uint premium;
         uint area;
         uint startTime;
@@ -56,18 +45,18 @@ contract Agrinsure is ChainlinkClient, DateTime{
 
     mapping(address => uint[]) public userPolicies;  //user address to array of policy IDs
 
-    function newPolicy (uint _area, string _location, bool _forFlood, uint8 _cropId) external payable{
+    function newPolicy (uint _area, string memory _location, bool _forFlood, uint8 _cropId) external payable{
         require(msg.value == (cropTypes[_cropId].premiumPerAcre * _area),"Incorrect Premium Amount");
 
-        uint pId = policies.length++;
+        uint pId = policies.length + 1;
         userPolicies[msg.sender].push(pId);
         policy storage p = policies[pId];
 
-        p.user = msg.sender;
+        p.user = payable(msg.sender);
         p.premium = cropTypes[_cropId].premiumPerAcre * _area;
         p.area = _area;
-        p.startTime = now;
-        p.endTime = now + cropTypes[_cropId].duration * 30*24*60*60;  //converting months to seconds
+        p.startTime = block.timestamp;
+        p.endTime = block.timestamp + cropTypes[_cropId].duration * 30*24*60*60;  //converting months to seconds
         p.location = _location;
         p.coverageAmount = cropTypes[_cropId].coveragePerAcre * _area;
         p.forFlood = _forFlood;
@@ -75,82 +64,36 @@ contract Agrinsure is ChainlinkClient, DateTime{
         p.state = policyState.Active;
     }
 
-    function newCrop(uint8 _cropId,string _name, uint _premiumPerAcre, uint _duration, uint _coveragePerAcre) internal {
+    function newCrop(uint8 _cropId,string memory _name, uint _premiumPerAcre, uint _duration, uint _coveragePerAcre) internal {
         cropType memory c = cropType(_name, _premiumPerAcre, _duration, _coveragePerAcre);
         cropTypes[_cropId] = c;
     }
 
-    constructor(
-        address _link,
-        address _oracle,
-        bytes32 _jobId,
-        uint256 _oraclePaymentAmount
-        )
-    public
-    {
-        setChainlinkToken(_link);
-        setChainlinkOracle(_oracle);
-        jobId = _jobId;
-        oraclePaymentAmount = _oraclePaymentAmount;
+    Oracle public oracle;
+
+    constructor(address _oracle) {
+        oracle = Oracle(_oracle);
 
         newCrop(0, "rabi", 1, 6, 7);
         newCrop(1, "kharif", 2, 4, 10);
     }
 
-    function claim(uint _policyId, uint _timestamp) public {
+    function claim(uint _policyId) public {
         require(msg.sender == policies[_policyId].user, "User Not Authorized");
         require(policies[_policyId].state == policyState.Active, "Policy Not Active");
 
-        if(now > policies[_policyId].endTime)
+        if(block.timestamp > policies[_policyId].endTime)
         {
             policies[_policyId].state = policyState.TimedOut;
             revert("Policy's period has Ended.");
         }
-        // if(_timestamp < policies[_policyId].startTime)
-        //     revert("Insurance Not Covered by Policy");
-
-        string memory location = policies[_policyId].location;
-
-        uint16 year = getYear(_timestamp);
-        string memory y = uintToString(year);
-        uint16 month = getMonth(_timestamp);
-        string memory m = uintToString(month);
-        uint16 day = getDay(_timestamp);
-        string memory d = uintToString(day);
-
-        string memory date = y.toSlice().concat("-".toSlice());
-        date = date.toSlice().concat(m.toSlice());
-        date = date.toSlice().concat("-".toSlice());
-        date = date.toSlice().concat(d.toSlice());
-
-        claimPolicyId = _policyId;
-        makeRequest(location, date);
-
+        
+        queryOracle(_policyId, policies[_policyId].location);
     }
 
-    function makeRequest(string _location, string _date) internal returns (bytes32 requestId)
-    {
-        Chainlink.Request memory req = buildChainlinkRequest(jobId, this, this.fulfill.selector);
-        req.add("q", _location);      //location
-        req.add("date", _date); //format: yyyy-MM-dd
-        req.add("tp", "24");           //timeperiod 24hrs
-        req.add("copyPath", "data.weather.0.hourly.0.precipMM"); //get that day's precipitation in mm
-        requestId = sendChainlinkRequestTo(chainlinkOracleAddress(), req, oraclePaymentAmount);
-    }
+    function queryOracle(uint claimPolicyId, string memory location) internal {
+        uint _result = oracle.getPrecipitation(location);
 
-    function resetResult() internal
-    {
-        resultReceived = false;
-        //result = 0;
-        claimPolicyId = 0;
-    }
-
-    function fulfill(bytes32 _requestId, int256 _result)
-    public
-    recordChainlinkFulfillment(_requestId)
-    {
-        resultReceived = true;
-        //result = _result;
         uint payoutAmount;
 
         if(policies[claimPolicyId].forFlood)
@@ -189,26 +132,5 @@ contract Agrinsure is ChainlinkClient, DateTime{
                 policies[claimPolicyId].state = policyState.PaidOut;
             }
         }
-
-        resetResult();
-    }
-    
-    //Fallback function to implement the ability for the Contract Address to Accept Ether
-    function() public payable {}
-
-    function uintToString(uint v) pure internal returns (string str) {
-        uint maxlength = 100;
-        bytes memory reversed = new bytes(maxlength);
-        uint i = 0;
-        while (v != 0) {
-            uint remainder = v % 10;
-            v = v / 10;
-            reversed[i++] = byte(48 + remainder);
-        }
-        bytes memory s = new bytes(i);
-        for (uint j = 0; j < i; j++) {
-            s[j] = reversed[i - 1 - j];
-        }
-        str = string(s);
     }
 }
